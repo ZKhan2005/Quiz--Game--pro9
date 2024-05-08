77eb94f5a18c1890072854efa220a1337396041d
zeenatkhan11 <zeenatkhan14sep@gmail.com>
2024-05-08 23:52:22 +0500

    Quiz Fun Game

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const apiLink = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
+let fetchData = async (data) => {
+    let fetchQuiz = await fetch(data);
+    let res = await fetchQuiz.json();
+    return res.results;
+};
+let data = await fetchData(apiLink);
+let startQuiz = async () => {
+    let score = 0;
+    //for user name
+    let name = await inquirer.prompt({
+        type: "input",
+        name: "fname",
+        message: chalk.bold.blue(" <<====== Type Your Name ? ======>>")
+    });
+    for (let i = 1; i <= 5; i++) {
+        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+        let ans = await inquirer.prompt({
+            type: 'list',
+            name: 'quiz',
+            message: data[i].question,
+            choices: answers.map((val) => val),
+        });
+        if (ans.quiz == data[i].correct_answer) {
+            ++score;
+            console.log(chalk.bold.magentaBright('!!..... Correct Answer .....!!'));
+        }
+        else {
+            console.log(chalk.bold.green `*====== Correct Answer Is  ${chalk.bold.green(data[i].correct_answer)} ======*`);
+        }
+    }
+    console.log(chalk.bold.yellow `Player ${chalk.blueBright.bold(name.fname)}  Your Score Is ${chalk.bold.redBright(score)} out of ${chalk.bold.bgCyan(` ***   5   ***`)}`);
+};
+startQuiz();
